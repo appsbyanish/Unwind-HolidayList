5a2e63284619e31476eb21b59e02e0c32cfed62e
Anishgopal Venugopal <>
2018-11-18 01:07:21 +0530

    update colours

--- Unwind - Holiday List/Constants.swift
+++ Unwind - Holiday List/Constants.swift
@@ -11,7 +11,7 @@ import UIKit
 
 struct Constants {
     static let TIME_INTERVAL_ONE_DAY: Double = 86400
-    static let HOLIDAY_LIST_CELL_COLOR = ["#9AECDB", "#BDEEE6", "#EBECEE"]
-    static let HOLIDAY_LIST_HIDDEN_COLOR = UIColor.darkGray
+    static let HOLIDAY_LIST_CELL_COLOR = ["#9AECDB", "#BDEEE6", "#e1e1e1"]
+    static let HOLIDAY_LIST_HIDDEN_COLOR = UIColor.init(hexString: "#a0a0a0")
     //static let HOLIDAY_LIST_CELL_COLOR_ALTERNATE =
 }
